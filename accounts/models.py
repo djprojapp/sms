cf8d0ec39030a8a901beccaa024f8b85b978f907
djprojapp <mjamil13@gmail.com>
2024-02-21 16:24:28 +0500

    Pak studies

--- accounts/models.py
+++ accounts/models.py
@@ -7,14 +7,14 @@ from PIL import Image
 class UserProfile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     institute=models.CharField(max_length=200, null=True, blank=True, default="")
-    img=models.ImageField(upload_to='images', default='default_pic.jpg')
+    img=models.ImageField(upload_to='images', default='default_pic.png', blank=True)
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         image=Image.open(self.img.path)
 
-        if image.height>300 or image.width>300:
-            output_size= (300, 300)
+        if image != "" and (image.height>250 or image.width>150):
+            output_size= (250, 150)
             image.thumbnail(output_size)
             image.save(self.img.path)
 
